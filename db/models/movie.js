b390fa96d6616d9f8a32a2db2572a92484ac6d0c
Jairo <jairoh28@gmail.com>
2020-12-28 17:45:32 -0600

    Initial commit

---
+++ db/models/movie.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Movie extends Sequelize.Model {}
+  Movie.init({
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    releaseYear: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+  }, { sequelize });
+
+  Movie.associate = (models) => {
+    Movie.belongsTo(models.Person, {
+      as: 'director', // alias to display data as
+      foreignKey: {
+        fieldName: 'directorPersonId',
+        allowNull: false,
+      },
+    });
+  };
+
+  return Movie;
+};
